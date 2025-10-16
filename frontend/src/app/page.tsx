f50919161ced91da6faa74c3967b3c3ae902bff6
sylv <sydauria@student.42.fr>
2025-10-16 08:04:09 +0200

    Enhance AlertDialog: support direct open with <useTrigger={false}> and <initialOpen={true}>, custom trigger via <trigger={...}>, optional confirm/cancel buttons (<confirm-cancel={false}>), customizable button text (<confirmEdit>, <cancelEdit>), 5 theme variants (default, alert, confirm, alertIcon, confirmIcon), and custom icons via <iconEdit>

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,24 @@
 "use client"
 
 import { Hero } from "@/components/hero/Hero"
+import { Alert } from "@/components/atoms/AlertDialog"
+import { BookOpenTextIcon  } from "@phosphor-icons/react"
+
 
 export default function LandingPage() {
+  const title: string = "AlertDialog Usage:";
+  const description: string = "Alert Dialog can be open by click on a trigger or can be open directly when componant is rendering. To activate direct display <useTrigger={false}> and <initialOpen={true}>.\
+  If you want to use your own composant as trigger: <trigger={<your_component>}. To remove Cancel/Confirm button <confirm-cancel={false}>. You can also editing the displayed text in this button <confirmEdit=\"Edit you button text\">\
+   / cancelEdit=[..]. 5 variants themes are available (default | alert | confirm | alertIcon | confirmIcon). And you can add you own icon using <iconEdit={<your_own_icon>}"
+  
   return (
     <>
       <Hero />
+    <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black p-6 rounded shadow-lg">
+      <Alert trigger={<BookOpenTextIcon size={50}/>} title={title} description={description} variant="default" confirm_cancel={true} />
+      </div>
+        
     </>
+
   )
 }
