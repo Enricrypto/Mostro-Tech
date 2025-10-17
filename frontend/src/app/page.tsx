8a2620a1b76bf17b44aa62c14ef1e503c8781b71
sylv <sydauria@student.42.fr>
2025-10-17 03:40:49 +0200

    Laderboard with default and shadow variant. Display rank, avatar, name, amount and token

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,44 +3,17 @@
 import React from "react"
 import { Hero } from "@/components/hero/Hero"
 import { Alert } from "@/components/atoms/AlertDialog"
-import { ArtistFullCard } from "@/components/display/ArtistFullCard"
-import { BookOpenTextIcon  } from "@phosphor-icons/react"
+import { BookOpenTextIcon } from "@phosphor-icons/react"
 import { artistsData, getRandomArtist } from "@/data/artists"
-
-
+import { Leaderboard } from "@/components/molecules/Leaderboard/LeaderBoard"
+import { Avatar } from "@/components/atoms/Avatar"
+ 
 export default function LandingPage() {
-  const title: string = "AlertDialog Usage:";
-  const description: string = "Alert Dialog can be open by click on a trigger or can be open directly when componant is rendering. To activate direct display <useTrigger={false}> and <initialOpen={true}>.\
-  If you want to use your own composant as trigger: <trigger={<your_component>}. To remove Cancel/Confirm button <confirm-cancel={false}>. You can also editing the displayed text in this button <confirmEdit=\"Edit you button text\">\
-   / cancelEdit=[..]. 5 variants themes are available (default | alert | confirm | alertIcon | confirmIcon). And you can add you own icon using <iconEdit={<your_own_icon>}"
-  
-  // Récupérer un artiste aléatoire ou le premier artiste
-  const featuredArtist = getRandomArtist()
-  
   return (
     <div>
       <Hero />
       
-      {/* ArtistFullCard Component with data from file */}
-      <div className="mt-8">
-        <ArtistFullCard 
-          artist={featuredArtist}
-        />
+        <Leaderboard rank={1} avatar={<Avatar src="/avatar.png" />} name="Daft Puuuuuuuuuuuuuuuuuuuuuuuunk" volume={100000000000} token="SOL" variant="shadow" ></Leaderboard>
       </div>
-      
-      {/* Affichage de plusieurs artistes */}
-      <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {artistsData.slice(0, 3).map((artist) => (
-          <ArtistFullCard 
-            key={artist.id}
-            artist={artist}
-          />
-        ))}
-      </div>
-      
-      <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black p-6 rounded shadow-lg">
-        <Alert trigger={<BookOpenTextIcon size={50}/>} title={title} description={description} variant="default" confirm_cancel={true} />
-      </div>
-    </div>
-  )
+      )
 }
