a108f081604beccb5203dbb1784db821cc70c9d1
Enricrypto <105727501+Enricrypto@users.noreply.github.com>
2025-10-28 23:50:55 +0100

    fix: refactoring

--- frontend/src/app/all-artists/page.tsx
+++ frontend/src/app/all-artists/page.tsx
@@ -14,7 +14,7 @@ export default function AllArtists() {
   return (
     <div className='bg-[#0A111F] min-h-screen w-full flex flex-col'>
       {/* ===== BADGES SECTION ===== */}
-      <section className='top-[149px] -ml-[50vw] -mr-[50vw] border-t-2 border-b-2 border-[#121B2B] bg-[#0A111FE5] backdrop-blur-sm py-5 px-4 mt-10'>
+      <section className='top-[149px] -ml-[50vw] -mr-[50vw] border-t-2 border-b-2 border-[#121B2B] bg-[#0A111FE5] backdrop-blur-sm py-5 px-4 mt-20'>
         <div className='max-w-[1200px] mx-auto'>
           <BadgesRow />
         </div>
