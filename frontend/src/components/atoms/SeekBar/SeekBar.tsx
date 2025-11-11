f8fe332f8d9bee592b735d6f3b5a579c631bff75
eibarraf <105727501+Enricrypto@users.noreply.github.com>
2025-11-11 10:12:01 +0100

    fix: refactor sizing atoms (#159)

--- frontend/src/components/atoms/SeekBar/SeekBar.tsx
+++ frontend/src/components/atoms/SeekBar/SeekBar.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const seekBarCVA = cva(
-  "relative w-full max-w-[533px] h-4 touch-none select-none flex items-center",
+  "relative w-full max-w-[33.3rem] touch-none select-none flex items-center",
   {
     variants: {
       variant: {
@@ -22,10 +22,14 @@ const seekBarCVA = cva(
 )
 
 const seekBarThumbCVA = cva(
-  "block w-4 h-4 rounded-full bg-white shadow-md cursor-pointer"
+  `
+  block w-[1rem] h-[1rem] sm:w-[1.1rem] sm:h-[1.1rem]
+  rounded-full bg-[var(--color-white)] shadow-md cursor-pointer
+  hover:scale-110 active:scale-95 transition-transform duration-150
+  `
 )
 
-const seekBarRangeCVA = cva("absolute h-full rounded-[40px]", {
+const seekBarRangeCVA = cva("absolute h-full rounded-full", {
   variants: {
     variant: {
       blue: "bg-[var(--color-skyblue)]",
@@ -59,7 +63,7 @@ export function SeekBar({
       step={0.1}
       onValueChange={(val) => onChange(val[0] / 100)}
     >
-      <SliderPrimitive.Track className='relative flex-1 h-4 rounded-[40px] bg-[rgba(255,255,255,0.2)]'>
+      <SliderPrimitive.Track className='relative flex-1 h-3 sm:h-[0.9rem] md:h-4 rounded-full bg-[rgba(255,255,255,0.2)]'>
         <SliderPrimitive.Range className={seekBarRangeCVA({ variant })} />
       </SliderPrimitive.Track>
       <SliderPrimitive.Thumb className={seekBarThumbCVA()} />
